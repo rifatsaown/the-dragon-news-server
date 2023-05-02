cdf560291672c641394025cd1948a04252e3bb44
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-05-02 10:50:55 +0600

    News Added git Push

---
+++ firebase/firebase.config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBHkcRkD-NvXK1AgPNVcqvaAwZm8AORR5A",
+  authDomain: "the-news-dragon-39400.firebaseapp.com",
+  projectId: "the-news-dragon-39400",
+  storageBucket: "the-news-dragon-39400.appspot.com",
+  messagingSenderId: "523948278785",
+  appId: "1:523948278785:web:f97dfd88ddd2cffb232c00"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- index.js
+++ index.js
@@ -6,15 +6,37 @@ const port = 3000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/news', (req, res) => {
+  res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find(item => item._id === id);
+  res.send(selectedNews)
+})
+
+app.get('/category/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id === 0) {
+    res.send(news)
+  }
+  else {
+    const categoryNews = news.filter(item => parseInt(item.category_id) === id);
+    res.send(categoryNews)
+  }
+})
+
 app.get('/categories', (req, res) => {
-    res.send(categories)
+  res.send(categories)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Dragon app listening on port ${port}`)
 })
